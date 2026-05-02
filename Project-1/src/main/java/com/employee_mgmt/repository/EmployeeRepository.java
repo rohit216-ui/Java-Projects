c0d882df0316102b6d76c9ca38b1a7c596ee3955
Rohit Joshi <107613578+rohit216-ui@users.noreply.github.com>
2026-05-02 14:02:51 +0530

    Delete Project-1 directory

--- Project-1/src/main/java/com/employee_mgmt/repository/EmployeeRepository.java
+++
@@ -1,7 +0,0 @@
-package com.employee_mgmt.repository;
-
-import com.employee_mgmt.model.Employee;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface EmployeeRepository extends JpaRepository<Employee, Long> {
-}
